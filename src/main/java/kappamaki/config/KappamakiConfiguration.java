95543e942d0dab618c728fa0b85c24fbdbfdcfc5
Brian Laframboise <brian.laframboise@gmail.com>
2011-08-15 20:52:08 -0400

    Externalized configuration of project and features directories

--- src/main/java/kappamaki/config/KappamakiConfiguration.java
+++ src/main/java/kappamaki/config/KappamakiConfiguration.java
@@ -1,30 +1,32 @@
 package kappamaki.config;
 
-import static kappamaki.util.Utils.USER_DIR;
-
 import java.io.File;
 
 import kappamaki.execute.ProcessExecutor;
 import kappamaki.execute.ScenarioExecutor;
 import kappamaki.index.Index;
 import kappamaki.index.Indexer;
-import kappamaki.util.Utils;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KappamakiConfiguration {
 
+    @Value("#{systemProperties['kappamaki.features']}")
+    private File features;
+
+    @Value("#{systemProperties['kappamaki.project']}")
+    private File project;
+
     @Bean
     public Index index() {
-        File features = Utils.joinPaths(USER_DIR, "example", "features");
         return new Indexer(features).index();
     }
 
     @Bean
     public ScenarioExecutor scenarioExecutor() {
-        File project = Utils.joinPaths(USER_DIR, "example");
         return new ProcessExecutor(index(), project);
     }
 }
