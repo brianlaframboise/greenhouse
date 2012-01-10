6598bc01d311286085f716ccf4674285b7402fae
brianlaframboise <brian.laframboise@gmail.com>
2012-01-10 17:59:46 -0500

    Added custom IConverter for dates

--- src/main/java/greenhouse/ui/wicket/GreenhouseApplication.java
+++ src/main/java/greenhouse/ui/wicket/GreenhouseApplication.java
@@ -3,10 +3,17 @@ package greenhouse.ui.wicket;
 import greenhouse.ui.wicket.page.FeaturesPage;
 import greenhouse.ui.wicket.page.ProjectsPage;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.apache.wicket.IConverterLocator;
 import org.apache.wicket.Page;
 import org.apache.wicket.markup.html.JavascriptPackageResource;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
+import org.apache.wicket.util.convert.ConverterLocator;
+import org.apache.wicket.util.convert.IConverter;
 import org.wicketstuff.annotation.scan.AnnotatedMountScanner;
 
 import com.jquery.JQueryResourceReference;
@@ -24,6 +31,23 @@ public class GreenhouseApplication extends WebApplication {
     @Override
     public Class<? extends Page> getHomePage() {
         return ProjectsPage.class;
+    }
+
+    @Override
+    protected IConverterLocator newConverterLocator() {
+        ConverterLocator locator = (ConverterLocator) super.newConverterLocator();
+        locator.set(Date.class, new IConverter() {
+            @Override
+            public String convertToString(Object value, Locale locale) {
+                return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss a").format((Date) value);
+            }
+
+            @Override
+            public Object convertToObject(String value, Locale locale) {
+                throw new UnsupportedOperationException();
+            }
+        });
+        return locator;
     }
 
 }
